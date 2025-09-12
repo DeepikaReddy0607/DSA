4cdef88e952d5000abe12f446b305d67bac6ec10
DeepikaReddy0607 <deepikareddyrudru@gmail.com>
2025-09-12 19:05:10 +0530

    Easy

---
+++ Day-5/Armstrong_number.java
@@ -0,0 +1,14 @@
+class Armstrong_number {
+    static boolean armstrongNumber(int n) {
+        int temp = n;
+        int sum = 0;
+        
+        while(n > 0){
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n = n / 10;
+        }
+        return sum == temp;
+        
+    }
+}
