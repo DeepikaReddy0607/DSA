8f193379d05ec657621a6fc7be51ce7c39188b0f
DeepikaReddy0607 <deepikareddyrudru@gmail.com>
2025-09-13 19:21:56 +0530

    Easy

---
+++ Day-6/Divisors.java
@@ -0,0 +1,11 @@
+public class Divisors {
+    public static void print_divisors(int n) {
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.print(n);
+    }
+}
+
